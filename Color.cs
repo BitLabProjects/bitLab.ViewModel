fdadcc8289ad63f4cea76e2daa404e61d5faf3e4
Xed89 <simone.bosc@gmail.com>
2016-02-26 22:36:30 +0100

    Addded alpha to color

--- Color.cs
+++ Color.cs
@@ -8,18 +8,19 @@ namespace bitLab.ViewModel
 {
   public class Color
   {
-    public byte R, G, B;
+    public byte R, G, B, A;
 
     public Color()
     {
-      R = G = B = 0;
+      R = G = B = A = 0;
     }
 
-    public Color(byte r, byte g, byte b)
+    public Color(byte r, byte g, byte b, byte a = 255)
     {
       R = r;
       G = g;
       B = b;
+      A = a;
     }
 
     public byte GetValue()
@@ -36,22 +37,22 @@ namespace bitLab.ViewModel
     public Color GetOpposite()
     {
       return new Color((byte)(255 - R), 
-                        (byte)(255 - G), 
-                        (byte)(255 - B));
+                       (byte)(255 - G), 
+                       (byte)(255 - B));
     }
 
     public static Color operator ^(Color c1, Color c2)
     {
       return new Color((byte)(c1.R ^ c2.R),
-                        (byte)(c1.G ^ c2.G),
-                        (byte)(c1.B ^ c2.B));
+                       (byte)(c1.G ^ c2.G),
+                       (byte)(c1.B ^ c2.B));
     }
 
     public Color AddAll(int value)
     {
       return new Color((byte)System.Math.Max(0, System.Math.Min(255, R + value)),
-                        (byte)System.Math.Max(0, System.Math.Min(255, G + value)),
-                        (byte)System.Math.Max(0, System.Math.Min(255, B + value)));
+                       (byte)System.Math.Max(0, System.Math.Min(255, G + value)),
+                       (byte)System.Math.Max(0, System.Math.Min(255, B + value)));
     }
   }
 }
